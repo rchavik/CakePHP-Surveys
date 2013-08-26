3aa10c899e6c1d657c445f34fb0b7d8f63ca46f6
Dimas Abdul Aziiz <dimas@xintesa.com>
2013-08-26 16:00:02 +0700

    update schema

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -20,11 +20,12 @@ class SurveysSchema extends CakeSchema {
 	public $questions = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
 		'survey_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10),
-		'type' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 10),
+		'type' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 50),
 		'questions' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'total_sequence' => array('type' => 'integer', 'null' =>true, 'default' => NULL),
+		'weight' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
 		'indexes' => array(
 			'id' => array('column' => array('id'), 'unique' => true)
 		),
@@ -35,15 +36,54 @@ class SurveysSchema extends CakeSchema {
 		),
 	);
 	
-	public $answers = array(
+	public $submissions = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
 		'user_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10),
-		'question_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10),
-		'answer' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10),
+		'raw_data' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'point' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'length' => 10),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
 		'indexes' => array(
 			'id' => array('column' => array('id'), 'unique' => true),
+		),
+		'tableParameters' => array(
+			'charset' => 'utf8',
+			'collate' => 'utf8_unicode_ci',
+			'engine' => 'InnoDb'
+		),
+	);
+
+	public $submission_details = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
+		'question_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10),
+		'submission_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10),
+		'sequence_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10),
+		'value' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'text' => array('type' => 'text', 'null' => false, 'default' => NULL),
+		'point' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'indexes' => array(
+			'id' => array('column' => array('id'), 'unique' => true),
+		),
+		'tableParameters' => array(
+			'charset' => 'utf8',
+			'collate' => 'utf8_unicode_ci',
+			'engine' => 'InnoDb'
+		),
+	);
+
+	public $question_options = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
+		'question_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10),
+		'sequence_id' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'length' => 10),
+		'options' => array('type' => 'string', 'null' => true, 'default' => NULL),
+		'weight' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'length' => 10),
+		'point' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'indexes' => array(
+			'id' => array('column' => array('id'), 'unique' => true)
 		),
 		'tableParameters' => array(
 			'charset' => 'utf8',
